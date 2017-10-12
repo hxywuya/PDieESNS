1c96fe057cf3dd9049fc464a039e6f1f1376c23c
PDieE <23275429@qq.com>
2017-10-12 16:42:18 +0800

    增加ztree——jq树插件

--- application/config.php
+++ application/config.php
@@ -143,6 +143,7 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str'       => [
         '__layui__' => '/static/layui-v2.1.5',
+        '__ztree__' => '/static/zTree_v3-3.5.29',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
